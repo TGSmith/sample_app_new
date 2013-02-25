c7a7f760393d0aa0bfc7fc0d2f48523e54fa3e26
Tristan Smith <Tristan13smith@gmail.com>
2013-02-25 04:46:10 +0000

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -25,10 +25,14 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
-
+    
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before { sign_in user }
+      before do
+        fill_in "Email", with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Sign in"
+      end
 
       it { should have_selector('title', text: user.name) }
 
@@ -37,7 +41,7 @@ describe "Authentication" do
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
-     
+
       describe "followed by signout" do
         before { click_link "Sign out" }
         it { should have_link('Sign in') }
@@ -66,6 +70,7 @@ describe "Authentication" do
 
           describe "when signing in again" do
             before do
+              delete signout_path
               visit signin_path
               fill_in "Email", with: user.email
               fill_in "Password", with: user.password
@@ -79,18 +84,6 @@ describe "Authentication" do
         end
       end
 
-      describe "in the Relationships controller" do
-        describe "submitting to the create action" do
-          before { post relationships_path }
-          specify { response.should redirect_to(signin_path) }
-        end
-
-        describe "submitting to the destroy action" do
-          before { delete relationship_path(1) }
-          specify { response.should redirect_to(signin_path) }
-        end
-      end
-
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -100,10 +93,10 @@ describe "Authentication" do
 
         describe "submitting to the update action" do
           before { put user_path(user) }
-          specify { response.should redirect_to(signin_path) }
+          specify { response.should redirect_to(signin_url) }
         end
 
-        describe "visiting the user index" do
+        describe "visiting user index" do
           before { visit users_path }
           it { should have_selector('title', text: 'Sign in') }
         end
@@ -118,18 +111,30 @@ describe "Authentication" do
           it { should have_selector('title', text: 'Sign in') }
         end
       end
-    end
 
-    describe "in the Microposts controller" do
+      describe "in the Microposts controller" do
 
-      describe "submitting to the create action" do
-        before { post microposts_path }
-        specify { response.should redirect_to(signin_path) }
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_url) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_url) }
+        end
       end
 
-      describe "submitting to the destroy action" do
-        before { delete micropost_path(FactoryGirl.create(:micropost)) }
-        specify { response.should redirect_to(signin_path) }
+      describe "in the Relationships controller" do
+        describe "submitting to the create action" do
+          before { post relationships_path }
+          specify { response.should redirect_to(signin_url) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete relationship_path(1) }
+          specify { response.should redirect_to(signin_url) }
+        end
       end
     end
 
@@ -140,12 +145,12 @@ describe "Authentication" do
 
       describe "visiting Users#edit page" do
         before { visit edit_user_path(wrong_user) }
-        it { should_not have_selector('title', text: full_title('Edit user')) }
+        it { should have_selector('title', text: full_title('')) }
       end
 
       describe "submitting a PUT request to the Users#update action" do
         before { put user_path(wrong_user) }
-        specify { response.should redirect_to(root_path) }
+        specify { response.should redirect_to(root_url) }
       end
     end
 
@@ -157,7 +162,7 @@ describe "Authentication" do
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
-        specify { response.should redirect_to(root_path) }
+        specify { response.should redirect_to(root_url) }
       end
     end
   end
